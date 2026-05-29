d68804fa6af1e5cc9f4130f081fa45124faab786
varitchaij-svg <varit.chaij@gmail.com>
2026-05-29 16:20:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbzTIGTE25c2qszy-XiLh9ZlDGCD3Lyg9dwEBiJqD9t4H1C--vDmx9NYdEghLEYJ_Sgk6w/exec";
-const SHEET_DEPLOY_URL = "https://script.google.com/macros/s/AKfycbzTIGTE25c2qszy-XiLh9ZlDGCD3Lyg9dwEBiJqD9t4H1C--vDmx9NYdEghLEYJ_Sgk6w/exec";
+const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbxsJeLzZoyXWH8zFm-Ijy4JPKuDa08iFJNSm1lm8dBxIRngfIlyOg8TfBBIx_5g9AKJ/exec";
+const SHEET_DEPLOY_URL = "https://script.google.com/macros/s/AKfycbxsJeLzZoyXWH8zFm-Ijy4JPKuDa08iFJNSm1lm8dBxIRngfIlyOg8TfBBIx_5g9AKJ/exec";
