5b4c84de30ecefcd804d3abc0568c1a4164efec3
varitchaij-svg <varit.chaij@gmail.com>
2026-05-31 15:36:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // ต้องตรงกับ Script Property ชื่อ APP_TOKEN ใน Apps Script; ปล่อยว่าง "" = ปิดการตรวจ
 const APP_TOKEN = "";
 
-const _SHEET_BASE = "https://script.google.com/macros/s/AKfycbyzBmF2cYl078rJH7Y9exA4-L7ZBR8UbebYo1VPUTbhuETI42lnAzrzJjOM1QJXUBYAMA/exec";
+const _SHEET_BASE = "https://script.google.com/macros/s/AKfycbyYt_Z_Ll_uJ4gco3j83pk9qCCLgHM0KW5mn9KGIXn2HMwvFs-zm3X6STTcBIzMKv4x/exec";
 // แนบ token เป็น query string → ใช้ได้ทั้ง GET (doGet) และ POST (e.parameter.token)
 const _SHEET_URL = _SHEET_BASE + (APP_TOKEN ? ("?token=" + encodeURIComponent(APP_TOKEN)) : "");
 
